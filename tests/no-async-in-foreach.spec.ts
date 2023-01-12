30bdaf60a8bae8a9bb69b98dde1766dcbbb79004
Thomas Prelot <thomasp@hokla.com>
2023-01-12 14:48:18 +0100

    new rule: no async in foreach

---
+++ tests/no-async-in-foreach.spec.ts
@@ -0,0 +1,31 @@
+import { ESLintUtils } from "@typescript-eslint/utils";
+import { rule } from "../lib/rules/no-async-in-foreach";
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+});
+
+ruleTester.run("{RULE_NAME}", rule, {
+  valid: [
+    `
+		campaign.auditees.forEach((auditee) => {
+			auditeesWithResponseDraftStatus.push({
+				...auditee,
+				hasResponseDraft: (responseService.findResponseByEvaluationId(auditee._id)) !== null,
+			});
+		});
+    `,
+  ],
+  invalid: [
+    {
+      code: `
+		campaign.auditees.forEach(async (auditee) => {
+			auditeesWithResponseDraftStatus.push({
+				...auditee,
+				hasResponseDraft: (await responseService.findResponseByEvaluationId(auditee._id)) !== null,
+			});
+		});      `,
+      errors: [{ messageId: "no-async-in-foreach" }],
+    },
+  ],
+});
