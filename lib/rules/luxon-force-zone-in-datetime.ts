7b901c4c382e65da2eb9bd59887e9681c15ac86c
Cyrielle Willerval <cyriellew@hokla.com>
2023-01-12 14:42:41 +0100

    add luxon rule and tests

---
+++ lib/rules/luxon-force-zone-in-datetime.ts
@@ -0,0 +1,68 @@
+import {
+  AST_NODE_TYPES,
+  ESLintUtils,
+  TSESTree,
+} from "@typescript-eslint/utils";
+
+type MessageIds = "missing-zone-in-datetime";
+
+type Options = [];
+
+const createRule = ESLintUtils.RuleCreator(
+  (name) => `https://hokla.com/rule/${name}`
+);
+
+// Trying to follow example from this blog :
+// https://ryankubik.com/blog/eslint-internal-state#Building-More-Complex-ESLint-Rules
+// https://astexplorer.net/#/gist/1ff99fca3f85c2e7676ac041a88d7b53/179cf88e3a77c133741d9f96f0dc982b9f11ce4d
+
+type NodeWithBody = TSESTree.Node & {
+  body: TSESTree.BlockStatement;
+};
+
+export const rule = createRule<Options, MessageIds>({
+  name: "luxon-force-zone-in-datetime",
+  defaultOptions: [],
+  create(context) {
+    return {
+      // Select a node from the AST
+      ["CallExpression[callee.property.name='fromISO']"](
+        node: TSESTree.CallExpression
+      ) {
+        const objectExpressionArgument = node.arguments.find(argument => argument.type === AST_NODE_TYPES.ObjectExpression);
+        
+        if (objectExpressionArgument === undefined) {
+          return context.report({
+            messageId: 'missing-zone-in-datetime',
+            node: node,
+          });
+        }
+
+        const hasZoneProperty = ((objectExpressionArgument as unknown as TSESTree.ObjectExpression)
+          .properties
+          .find(property => ((property as TSESTree.Property).key as TSESTree.Identifier).name === 'zone')) !== undefined;
+        
+        if (!hasZoneProperty) {
+          return context.report({
+            messageId: 'missing-zone-in-datetime',
+            node: node,
+          });
+        }
+      },
+    };
+  },
+  meta: {
+    docs: {
+      recommended: "warn",
+      description:
+        "TODO",
+    },
+    messages: {
+      "missing-zone-in-datetime": 'TODO'
+    },
+    type: "problem",
+    schema: [],
+  },
+});
+
+export default rule;
