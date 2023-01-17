ccacfde01bddf8b50c5d557d7eb00ece4670d450
Thomas Walter <thomasw@hokla.com>
2023-01-17 10:01:08 +0100

    style: add some line breaks in description

--- lib/rules/redux-saga-no-sequential-actions.ts
+++ lib/rules/redux-saga-no-sequential-actions.ts
@@ -32,11 +32,11 @@ export const rule = createRule<Options, MessageIds>({
     docs: {
       recommended: "warn",
       description:
-        "Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states. Prefer using a single 'event'-type action that respects domain/name or you can use batch() to batch multiple actions together. See standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially. See defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a",
+        "Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states. Prefer using a single 'event'-type action that respects domain/name or you can use batch() to batch multiple actions together. \n\nSee standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially. \nSee defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a",
     },
     messages: {
       "sequential-redux-actions":
-        "Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states.\n\nPrefer using a single 'event'-type action that respects domain/ name or you can use batch() to batch multiple actions together. See standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially and known defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a",
+        "Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states.\n\nPrefer using a single 'event'-type action that respects domain/ name or you can use batch() to batch multiple actions together. \n\nSee standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially. \nSee defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a",
     },
     type: "problem",
     schema: [],
