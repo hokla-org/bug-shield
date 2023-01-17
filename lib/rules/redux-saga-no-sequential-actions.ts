71caeef463dc20ef31fe0a961c942f60e427894d
Thomas Walter <thomasw@hokla.com>
2023-01-17 10:03:40 +0100

    refactor: put description in a variable

--- lib/rules/redux-saga-no-sequential-actions.ts
+++ lib/rules/redux-saga-no-sequential-actions.ts
@@ -5,6 +5,11 @@ import {
 } from "@typescript-eslint/utils";
 
 type MessageIds = "sequential-redux-actions";
+const DESCRIPTION = `Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states.
+Prefer using a single 'event' - type action that respects domain / name or you can use batchActions() from 'redux-batched-actions' to batch multiple actions together.
+
+See standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially. 
+See defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a`;
 
 type Options = [];
 
@@ -31,12 +36,10 @@ export const rule = createRule<Options, MessageIds>({
   meta: {
     docs: {
       recommended: "warn",
-      description:
-        "Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states. Prefer using a single 'event'-type action that respects domain/name or you can use batch() to batch multiple actions together. \n\nSee standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially. \nSee defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a",
+      description: DESCRIPTION,
     },
     messages: {
-      "sequential-redux-actions":
-        "Avoid dispatching two actions sequentially as it will result in multiple UI updates, and unexpected display due to illegal intermediate states.\n\nPrefer using a single 'event'-type action that respects domain/ name or you can use batch() to batch multiple actions together. \n\nSee standard https://redux.js.org/style-guide/#avoid-dispatching-many-actions-sequentially. \nSee defect https://www.notion.so/m33/Timebox-1h-APP-Screen-is-blinking-while-logging-out-of-in-clinic-mode-55e774890d3b414786dd0064e91ce70a",
+      "sequential-redux-actions": DESCRIPTION,
     },
     type: "problem",
     schema: [],
