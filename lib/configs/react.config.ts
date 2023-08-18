097c920ede034779f6bd67b416c1b352a1ff43fa
Alexandre Pierre <alexandrep@hokla.com>
2023-08-18 15:07:04 +0200

    feat(reactConfig): create react config

---
+++ lib/configs/react.config.ts
@@ -0,0 +1,29 @@
+import { RuleListener, RuleModule } from "@typescript-eslint/utils/dist/ts-eslint";
+import { Config, PLUGIN_NAME, getConfigCustomRules } from "../utils/config";
+
+export const CONFIG_NAME = "react"
+
+export const getConfig = (
+  allRules: { [ruleName: string]: RuleModule<string, [], RuleListener> & { configs : string[]}}
+): { [configName: string]: Config} => {
+  return {
+    [CONFIG_NAME]: {
+      extends: [
+        "plugin:@hokla/custom-rules/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+      ],
+      plugins: [
+        PLUGIN_NAME,
+      ],
+      rules: {
+        ...getConfigCustomRules(CONFIG_NAME, allRules),
+      },
+      parserOptions: {
+        "ecmaFeatures": {
+          "jsx": true
+        },
+      },
+    },
+  }
+}
