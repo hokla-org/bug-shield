6d9668390522efc9f0e7e0ce9077e1a184121b9b
Alexandre Pierre <alexandrep@hokla.com>
2023-11-08 15:13:33 +0100

    test(svgAttributesRule): tested mandatory-attributes-for-svg-elements rule

---
+++ tests/rules/mandatory-attributes-for-svg-elements.spec.ts
@@ -0,0 +1,65 @@
+import { ESLintUtils } from "@typescript-eslint/utils";
+import rule from "../../lib/rules/mandatory-attributes-for-svg-elements";
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+});
+
+const activateJsxParsingOption = {
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+ruleTester.run("{RULE_NAME}", rule, {
+  valid: [
+    {
+      code: `
+    <Svg height="100" width="100" viewBox="0 0 100 100"></Svg>
+    `,
+      parserOptions: { ...activateJsxParsingOption },
+    },
+    {
+      code: `
+      <svg height={height} width={width} viewBox={viewBox}/>
+    `,
+      parserOptions: { ...activateJsxParsingOption },
+    },
+    {
+      code: `
+      <svg {...props}/>
+    `,
+      parserOptions: { ...activateJsxParsingOption },
+    },
+  ],
+  invalid: [
+    {
+      code: `
+      <svg />
+      `,
+      errors: [{ messageId: "mandatory-attributes-for-svg-elements" }],
+      parserOptions: { ...activateJsxParsingOption },
+    },
+    {
+      code: `
+      <Svg width={width} viewBox={viewBox}/>
+      `,
+      errors: [{ messageId: "mandatory-attributes-for-svg-elements" }],
+      parserOptions: { ...activateJsxParsingOption },
+    },
+    {
+      code: `
+      <svg viewBox={viewBox}/>
+      `,
+      errors: [{ messageId: "mandatory-attributes-for-svg-elements" }],
+      parserOptions: { ...activateJsxParsingOption },
+    },
+    {
+      code: `
+      <Svg height="100" width={width}/>
+      `,
+      errors: [{ messageId: "mandatory-attributes-for-svg-elements" }],
+      parserOptions: { ...activateJsxParsingOption },
+    },
+  ],
+});
